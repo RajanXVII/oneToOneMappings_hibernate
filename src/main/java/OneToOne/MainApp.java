590edab01942260837809cfd12c5128ae04dddb1
'RajanXVII' <a839348@DESKTOP-DN1RNVP>
2022-06-25 20:51:33 +0530

    Delete functionality for one to one mappings

--- src/main/java/OneToOne/MainApp.java
+++ src/main/java/OneToOne/MainApp.java
@@ -1,6 +1,7 @@
 package OneToOne;
 
 import java.sql.SQLException;
+import java.util.Objects;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -16,11 +17,19 @@ public class MainApp {
 		
 		Session sess=sf.getCurrentSession();
 		sess.beginTransaction();
-		Instructor instructor=new Instructor("rajan", "sonvane", "rajasonvane17.rs@gmail.com");
-		InstructorDetail instructorDetail=new InstructorDetail("rajanyt", "coding");
-		instructor.setInstructorDetail(instructorDetail);
-		sess.save(instructor);
-		System.out.println("persisted!!");
+		/*
+		 * Instructor instructor=new Instructor("rajan", "sonvane",
+		 * "rajasonvane17.rs@gmail.com"); InstructorDetail instructorDetail=new
+		 * InstructorDetail("rajanyt", "coding");
+		 */
+		int id=4;
+		Instructor instructor=sess.get(Instructor.class, id);
+		//instructor.setInstructorDetail(instructorDetail);
+		//sess.save(instructor);
+		sess.delete(instructor);
+		if(Objects.nonNull(instructor)) {
+			System.out.println("deleted!!");
+		}
 		sess.getTransaction().commit();
 	}
 }
